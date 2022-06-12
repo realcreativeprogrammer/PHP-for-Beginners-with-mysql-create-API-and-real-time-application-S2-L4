17ee981e49e52c4342852d23c880550b4e15cb67
realcreativeprogrammer <realcreativeprogrammer@gmail.com>
2022-06-12 10:34:31 +0300

    first

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+$myvar=NULL;
+echo gettype($myvar);
+echo '<br>';
+$myvar=3232.454;
+echo gettype($myvar);
+echo '<br>';
+
+$MYVAR=true;
+echo gettype($MYVAR);
+echo '<br>';
+$MYVAR=false;
+
+?>
